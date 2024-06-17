44751a667790378e30c5a846346d51aa255ed588
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2024-06-17 15:17:46 +0200

    ajous mise a jou

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
-
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
